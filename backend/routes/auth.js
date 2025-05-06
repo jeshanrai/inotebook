2daa4a6fe67204bb385f14490a2dca9ba3fc035c
jeshanrai <jeshanrai96@gmail.com>
2025-05-06 22:05:19 +0545

    validaiton

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+// POST route
+router.post(
+  '/',
+  [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-//create a user using: POST "/api/auth/createuser" . No login required
+    try {
+        let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: 'User with this email already exists' });
+      }
+    
+      const { name, email, password } = req.body;
 
+      user = new User({ name, email, password });
+      await user.save();
 
-
-router.get('/', (req, res) => {
-    res.send('Hello World! this is my first express app');
-console.log(req.body);	
-const user =User(req.body);
-user.save()
-res.send(req.body);
-
-}
+      res.status(201).json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
 );
+
 module.exports = router;
