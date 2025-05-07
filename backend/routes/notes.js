f796c77c1e8f7a420fb646578b3ff73cf3c90faa
jeshanrai <jeshanrai96@gmail.com>
2025-05-07 23:30:54 +0545

    add notes and fetch notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send('Hello World! first express app');
-}
-);
+var fetchuser = require('../middleware/fetchuser');
+ const Notes = require('../models/Notes');   
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id });
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// Route 2: Add a new note using POST "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        });
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
 module.exports = router;
